129f89ff5804e00742c83421a3efd011e98d3743
Abdul Basit <basit.saleem13@gmail.com>
2024-01-23 22:43:52 +0500

    feat: user model is added and db is connected

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
+const connectDb = require('./db/connect')
 const express = require('express')
 const app = express()
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> {
-    console.log(`server is listening on PORT ${port}`)
-})
+
+const start = async () => {
+    try {
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port, ()=> {
+            console.log(`server is listening on PORT ${port}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
